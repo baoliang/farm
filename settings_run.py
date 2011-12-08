7b48d5329dd70ac0e37ba3b12fa058d9df8149e9
baoliang <jfreebird@gmail.com>
2011-12-08 21:21:47 +0800

    实现新的UI

---
+++ settings_run.py
@@ -0,0 +1,12 @@
+mongo_host = '127.0.0.1'
+mongo_port = 27017
+mongo_name = 'farm_dev'
+mysql_host = '127.0.0.1'
+mysql_port = 3306
+DEBUG = True
+MAKO_DIR = 'templates'
+PAGE_LIMIT = 100
+# optional, if specified Mako will cache to this directory
+MAKO_CACHEDIR = '/tmp/mako'
+# optional, if specified Mako will respect the cache size
+MAKO_CACHESIZE = 500
