a4d407e22d873198d676733386cc267d239c01bc
张保良 <jfreebird@gmail.com>
2012-03-26 17:59:25 +0800

    去除荣誉

--- dic.py
+++ dic.py
@@ -1,16 +1,6 @@
-level_html = {
-    '0': 'super_admin/index.html',
-    '1': 'index.html',
-    '2': 'admin/index.html',
-}
+farm  =  ｛
+    “sell”: {
+            “send”: "send/send_sell.html"
+         }
 
-
-collection_html = {
-
-}
-
-
-args = {
-    'False': False,
-    'True': True,
-}
+｝
